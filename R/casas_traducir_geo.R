d742079a5f825d14c7e11d0a4279a36f933cf9c7
Felipe Gonzalez <felipexgonzalez@gmail.com>
2024-11-04 16:02:08 -0600

    Complementar valores de shapley

--- R/casas_traducir_geo.R
+++ R/casas_traducir_geo.R
@@ -1,7 +1,7 @@
 ## Seleccionar variables para curso y poner nombres en español
 library(tidyverse)
 casas_f <- read_csv("../datos/houseprices/house-prices-train.csv") %>%
-  select(id = Id, tipo_zona = MSZoning, frente_lote = LotFrontage,
+  dplyr::select(id = Id, tipo_zona = MSZoning, frente_lote = LotFrontage,
          area_lote = LotArea,
          calle = Street, forma_lote = LotShape,
          nombre_zona = Neighborhood,
@@ -50,10 +50,10 @@ casas_f <- casas_f %>% mutate(area_sotano_m2 = area_sotano * m2,
                               area_hab_m2 = area_habitable_sup * m2,
                               area_garage_m2 = area_garage * m2,
                               area_lote_m2 = area_lote * m2) %>%
-  select(-area_sotano, -area_1er_piso, -area_2o_piso, -area_habitable_sup,
+  dplyr::select(-area_sotano, -area_1er_piso, -area_2o_piso, -area_habitable_sup,
          -area_garage, -area_lote) %>%
   mutate(precio_miles = precio / 1000, valor_misc_miles = valor_misc / 1000) %>%
-  select(-precio, -valor_misc) %>%
+  dplyr::select(-precio, -valor_misc) %>%
   mutate(precio_m2_miles = precio_miles / area_hab_m2)
 
 casas <- casas_f
